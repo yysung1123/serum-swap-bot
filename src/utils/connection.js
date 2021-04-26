0b30f108e7a4fe971722dab4d27af5741935a783
Yuan-Yao Sung <sungyuanyao@gmail.com>
2021-04-27 06:04:36 +0800

    Retry confirmTransaction when the response value is null

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,5 +1,5 @@
 import {
-    Transaction,
+  Transaction,
 } from "@solana/web3.js";
 
 import { delay } from './delay.js'
@@ -12,7 +12,7 @@ async function confirmTransaction (
   confirmations,
 ) {
   const start = Date.now();
-  const WAIT_TIMEOUT_MS = 20 * 1000;
+  const WAIT_TIMEOUT_MS = 10 * 1000;
 
   let statusResponse = await connection.getSignatureStatus(signature, {searchTransactionHistory: true});
   for (;;) {
@@ -41,52 +41,57 @@ async function confirmTransaction (
 
 // https://github.com/project-serum/oyster-swap/blob/7966c69c60c27b2c2f1346161ef993a3625e5982/src/utils/connection.tsx#L145
 export const sendTransaction = async (
-    connection,
-    wallet,
-    instructions,
-    signers,
-    awaitConfirmation = true
-  ) => {
-    let transaction = new Transaction();
-    instructions.forEach((instruction) => transaction.add(instruction));
-    transaction.recentBlockhash = (
-      await connection.getRecentBlockhash("max")
-    ).blockhash;
-    transaction.sign(wallet);
-    if (signers.length > 0) {
-      transaction.partialSign(...signers);
-    }
-    const rawTransaction = transaction.serialize();
+  connection,
+  wallet,
+  instructions,
+  signers,
+  awaitConfirmation = true
+) => {
+  let transaction = new Transaction();
+  instructions.forEach((instruction) => transaction.add(instruction));
+  transaction.recentBlockhash = (
+    await connection.getRecentBlockhash("max")
+  ).blockhash;
+  transaction.sign(wallet);
+  if (signers.length > 0) {
+    transaction.partialSign(...signers);
+  }
+  const rawTransaction = transaction.serialize();
 
-    let options = {
-      skipPreflight: true,
-      commitment: "confirmed",
-    };
-
-    const txid = await connection.sendRawTransaction(rawTransaction, options);
-
-    if (awaitConfirmation) {
-      //await connection.getConfirmedTransaction(txid, options && options.commitment);
-      let status;
-      for (;;) {
-        try {
-          status = (
-            await confirmTransaction(connection, txid, 5)
-          ).value;
-          break;
-        } catch (e) {
-          //console.log(e);
-        }
-      }
-
-      console.log(status);
-      if (status === null || status.err) {
-
-        throw new Error(
-          `Raw transaction ${txid} failed (${JSON.stringify(status)})`
-        );
-      }
-    }
-
-    return txid;
+  let options = {
+    skipPreflight: true,
+    commitment: "confirmed",
   };
+
+  const txid = await connection.sendRawTransaction(rawTransaction, options);
+
+  if (awaitConfirmation) {
+    //await connection.getConfirmedTransaction(txid, options && options.commitment);
+    let res, status, status_null_count = 0;
+    for (;;) {
+      try {
+        res = await confirmTransaction(connection, txid, 5);
+        status = res.value;
+        if (status === null && status_null_count < 2) {
+          console.log(`status null ${status_null_count}`);
+          console.log(txid);
+          status_null_count += 1;
+          continue;
+        }
+        break;
+      } catch (e) {
+        //console.log(e);
+      }
+    }
+
+    console.log(res);
+    if (status === null || status.err) {
+
+      throw new Error(
+        `Raw transaction ${txid} failed (${JSON.stringify(status)})`
+      );
+    }
+  }
+
+  return txid;
+};
